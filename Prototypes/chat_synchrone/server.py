41fbf10b2446f24bded59c49397676174a300951
Lucas-hrl <lucas.herchuel@uha.fr>
2025-11-15 10:13:46 +0100

    correction de la boucle while du serveur synchrone

--- Prototypes/chat_synchrone/server.py
+++ Prototypes/chat_synchrone/server.py
@@ -5,10 +5,10 @@ reply = "bye"
 print(host)
 data = ""
 
+server_socket = socket.socket()  # Création de la socket
+server_socket.bind(('0.0.0.0', port))  # Association du host et du port d'écoute
+server_socket.listen(1)  # attente de connexion
 while data != "arret":
-    server_socket = socket.socket() #Création de la socket
-    server_socket.bind(('0.0.0.0', port)) #Association du host et du port d'écoute
-    server_socket.listen(1) #attente de connexion
     conn, address = server_socket.accept() #etablissement de la connexion
     data = conn.recv(1024).decode() #réception et decodage des données
     print(data) #affiche le message
