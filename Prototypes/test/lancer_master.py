81f6b247b7c6ae7fde92cfa097ff79c4a39575fd
Lucas-hrl <lucas.herchuel@uha.fr>
2025-12-22 14:24:25 +0100

    Modification du script master et ajout de commentaires et d'une bdd

--- Prototypes/test/lancer_master.py
+++ Prototypes/test/lancer_master.py
@@ -1,31 +1,54 @@
+"""
+Script de lancement du Master.
+Usage : python lancer_master.py <PORT> [IP_ECOUTE]
+"""
+
 import sys
 import time
 from master_crypt import Master, get_ip
 
 if __name__ == "__main__":
-    # Récupérer les arguments ou utiliser les valeurs par défaut
-    if len(sys.argv) >= 3:
-        ip = sys.argv[1]
-        port = int(sys.argv[2])
-    elif len(sys.argv) == 2:
-        ip = get_ip()
+    # On force au moins un argument (le port)
+    if len(sys.argv) < 2:
+        print("Usage : python lancer_master.py <PORT> [IP_ECOUTE]")
+        print("Exemple : python lancer_master.py 9016")
+        print("Exemple : python lancer_master.py 9016 192.168.1.28")
+        sys.exit(1)
+
+    # Récupération des arguments
+    try:
         port = int(sys.argv[1])
+    except ValueError:
+        print("Erreur : Le port doit être un nombre entier.")
+        sys.exit(1)
+
+    # Si l'IP est donnée on la prend, sinon on détecte l'IP locale
+    if len(sys.argv) >= 3:
+        ip = sys.argv[2]
     else:
         ip = get_ip()
-        port = 9016
 
-    print(f"Démarrage du Master sur {ip}:{port}")
+    print(f"--- Lancement du Master sur {ip}:{port} ---")
+
+    # On instancie le Master
     master = Master(ip, port)
+
+    # Démarrage de l'écoute
     master.demarrer_ecoute()
 
-    # Garder le programme en vie
+    print(">> Master en ligne. En attente de routeurs...")
+    print(">> Ctrl+C pour arrêter.\n")
+
+    # Boucle infinie pour garder le programme actif
     try:
         while True:
             time.sleep(1)
     except KeyboardInterrupt:
         print("\nArrêt du Master...")
         master.running = False
-        # Fermer le socket pour débloquer l'accept()
         if master.master_socket_ecoute:
-            master.master_socket_ecoute.close()
-        print("Master arrêté.")
+            try:
+                master.master_socket_ecoute.close()
+            except:
+                pass
+        print("Master arrêté proprement.")
